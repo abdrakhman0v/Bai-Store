cd8447fabcb5a9b35ed2120c3917c07c8f436920
Loki <dimadimas19232@gmail.com>
2026-02-26 11:16:27 +0300

    Медиа добавил в настройки и лого в бренды

--- brands/models.py
+++ brands/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Brand(models.Model):
     name = models.CharField(max_length=100, unique=True, verbose_name="Название бренда")
     description = models.TextField(blank=True, null=True, verbose_name="Описание")
-    #logo = models.ImageField(upload_to='brands/', blank=True, null=True)
+    logo = models.ImageField(upload_to='brands/', blank=True, null=True)
 
     def __str__(self):
         return self.name
